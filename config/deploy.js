826949eab30f4a7a6b7d6e213227ee7ebb1b4040
Erika Pauwels <erika.pauwels@gmail.com>
2018-11-26 12:04:55 +0100

    Install and configure update, release and deploy dependencies

---
+++ config/deploy.js
@@ -0,0 +1,54 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(deployTarget) {
+  //see https://github.com/ember-cli-deploy/ember-cli-deploy-revision-data/issues/52
+  process.env.GIT_DISCOVERY_ACROSS_FILESYSTEM=1;
+
+  let ENV = {
+    build: {
+      environment: 'production'
+    },
+    'ssh-index': {
+      username: 'root',
+      host: 'dock.semte.ch',
+      remoteDir: '/data/lblod/dev-demo-editor/publicatie-app',
+      agent: process.env.SSH_AUTH_SOCK,
+      port: 2275,
+      allowOverwrite: true
+    },
+    'rsync': {
+      dest: '/data/lblod/dev-demo-editor/publicatie-app',
+      username: 'root',
+      host: 'dock.semte.ch',
+      port: 2275,
+      delete: false,
+      privateKey: process.env.SSH_AUTH_SOCK,
+      arg:['--verbose']
+    }
+  };
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    ENV['ssh-index'] = {
+      username: 'root',
+      host: 'dock.semte.ch',
+      remoteDir: '/data/lblod/demo-editor/publicatie-app',
+      agent: process.env.SSH_AUTH_SOCK,
+      port: 2275,
+      allowOverwrite: true
+    };
+
+    ENV['rsync'] = {
+      dest: '/data/lblod/demo-editor/publicatie-app',
+      username: 'root',
+      host: 'dock.semte.ch',
+      port: 2275,
+      delete: false,
+      privateKey: process.env.SSH_AUTH_SOCK,
+      arg:['--verbose']
+    };
+  }
+
+  return ENV;
+};
