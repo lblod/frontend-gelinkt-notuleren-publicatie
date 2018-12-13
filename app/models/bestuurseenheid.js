bea8449780bad698800e7fe4c7ec93b67d970177
Claire Lovisa <lovisa.claire@gmail.com>
2018-12-13 10:03:28 +0100

    Add bestuurseenheid and bestuurseenheid-classificatie-code models

---
+++ app/models/bestuurseenheid.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  import Model from 'ember-data/model';
+  import attr from 'ember-data/attr';
+  import { belongsTo, hasMany } from 'ember-data/relationships';
+
+  export default Model.extend({
+    naam: attr(),
+    classificatie: belongsTo('bestuurseenheid-classificatie-code', { inverse: null }),
+    bestuursorganen: hasMany('bestuursorgaan', { inverse: 'bestuurseenheid' }),
+    uri: attr()
+  });
+});
