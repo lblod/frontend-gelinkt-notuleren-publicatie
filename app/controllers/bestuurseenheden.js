41e69855f23908323db526c2e8477752b86fa678
Claire Lovisa <lovisa.claire@gmail.com>
2018-12-15 18:10:10 +0100

    Add an ember-power-select to select labels and naamen. The filtering is not working.

--- app/controllers/bestuurseenheden.js
+++ app/controllers/bestuurseenheden.js
@@ -1,4 +1,44 @@
 import Controller from '@ember/controller';
+import {
+  computed
+} from '@ember/object';
+import {
+  filter
+} from '@ember/object/computed';
 
 export default Controller.extend({
+  bestuurseenheidClassificatieLabels: computed(function() {
+    return this.model.getEach('classificatie').getEach('label').uniq();
+  }),
+
+  // The filter in not working at all.
+  //
+  // What is left to do : add a filter so that when the user selects a
+  // bestuurseenheidClassificatieLabel, the naamen are updated to fit with the
+  // selected label.
+
+  filteredBestuurseenheidNaamen: function() {
+    var selectedBestuurseenheidClassificatieLabel = this.get('selectedBestuurseenheidClassificatieLabel');
+    var bestuurseenheden = this.model;
+    if (selectedBestuurseenheidClassificatieLabel) {
+      return bestuurseenheden.filter(function(bestuurseenheid) {
+        return bestuurseenheid.get('classificatie.label') === selectedBestuurseenheidClassificatieLabel;
+      });
+    }
+    return bestuurseenheden.getEach('naam');
+  }.property('selectedBestuurseenheidClassificatieLabel'),
+
+  bestuurseenheidNaamen: computed(function() {
+    // return this.model.getEach('naam');
+    return this.get('filteredBestuurseenheidNaamen');
+  }),
+
+  actions: {
+    chooseBestuurseenheidClassificatieLabel(bestuurseenheidClassificatieLabel) {
+      this.set('bestuurseenheidClassificatieLabel', bestuurseenheidClassificatieLabel);
+    },
+    chooseBestuurseenheidNaam(bestuurseenheidNaam) {
+      this.set('bestuurseenheidNaam', bestuurseenheidNaam);
+    }
+  }
 });
