738a21136f43b13b069c4691afb809ac58924955
Claire Lovisa <lovisa.claire@gmail.com>
2018-12-17 10:06:47 +0100

    Fix the filtering of naamen when we select a label

--- app/controllers/bestuurseenheden.js
+++ app/controllers/bestuurseenheden.js
@@ -7,38 +7,25 @@ import {
 } from '@ember/object/computed';
 
 export default Controller.extend({
+  labelIsSelected: false,
+  naamIsSelected: false,
+
   bestuurseenheidClassificatieLabels: computed(function() {
     return this.model.getEach('classificatie').getEach('label').uniq();
   }),
 
-  // The filter in not working at all.
-  //
-  // What is left to do : add a filter so that when the user selects a
-  // bestuurseenheidClassificatieLabel, the naamen are updated to fit with the
-  // selected label.
-
-  filteredBestuurseenheidNaamen: function() {
-    var selectedBestuurseenheidClassificatieLabel = this.get('selectedBestuurseenheidClassificatieLabel');
-    var bestuurseenheden = this.model;
-    if (selectedBestuurseenheidClassificatieLabel) {
-      return bestuurseenheden.filter(function(bestuurseenheid) {
-        return bestuurseenheid.get('classificatie.label') === selectedBestuurseenheidClassificatieLabel;
-      });
-    }
-    return bestuurseenheden.getEach('naam');
-  }.property('selectedBestuurseenheidClassificatieLabel'),
-
-  bestuurseenheidNaamen: computed(function() {
-    // return this.model.getEach('naam');
-    return this.get('filteredBestuurseenheidNaamen');
+  bestuurseenheidNaamen: computed('bestuurseenheidClassificatieLabel', function() {
+    return this.model.filterBy('classificatie.label', this.bestuurseenheidClassificatieLabel).getEach('naam');
   }),
 
   actions: {
     chooseBestuurseenheidClassificatieLabel(bestuurseenheidClassificatieLabel) {
       this.set('bestuurseenheidClassificatieLabel', bestuurseenheidClassificatieLabel);
+      this.set('labelIsSelected', true);
     },
     chooseBestuurseenheidNaam(bestuurseenheidNaam) {
       this.set('bestuurseenheidNaam', bestuurseenheidNaam);
+      this.set('naamIsSelected', true);
     }
   }
 });
