17408a94c7bdb87234e20225a471d393cb916dce
Miet Claes <miet@miet.be>
2019-01-30 20:07:33 +0100

    Add new route for main zitting view with components and link to immediately

--- app/routes/bestuurseenheid/index.js
+++ app/routes/bestuurseenheid/index.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   afterModel(zittingen/*, transition*/) {
     if (zittingen.get('length') > 0) {
-      this.transitionTo('bestuurseenheid.zitting.agenda', zittingen.get('firstObject.id'));
+      this.transitionTo('bestuurseenheid.zitting.zitting', zittingen.get('firstObject.id'));
     }
   }
 });
