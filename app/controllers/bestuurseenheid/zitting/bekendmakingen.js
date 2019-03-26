05d3a5c8403c2f9c3f9b16bcdbe959ebfb6d6c63
Miet Claes <miet@miet.be>
2019-03-26 20:02:03 +0100

    TEMPORARY FOR STYLE make sure expanding and contracting works

--- app/controllers/bestuurseenheid/zitting/bekendmakingen.js
+++ app/controllers/bestuurseenheid/zitting/bekendmakingen.js
@@ -6,5 +6,11 @@ export default Controller.extend({
     'volgendUitBehandelingVanAgendapunt.position',
     'volgendUitBehandelingVanAgendapunt.onderwerp.position'
   ]),
-  besluiten: sort('model.besluiten', 'besluitenSort')
+  besluiten: sort('model.besluiten', 'besluitenSort'),
+
+  actions: {
+    toggleExpand(besluit) {
+      besluit.set('isExpanded', !besluit.get('isExpanded'));
+    }
+  }
 });
