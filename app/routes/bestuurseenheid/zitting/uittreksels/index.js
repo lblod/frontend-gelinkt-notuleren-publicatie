a1c4efecd21cc32112a9ae3990dcc484be168145
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-03-28 20:56:34 +0100

    rename route to index

--- app/routes/bestuurseenheid/zitting/uittreksels/index.js
+++ app/routes/bestuurseenheid/zitting/uittreksels/index.js
@@ -3,10 +3,11 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   init() {
     this._super(...arguments);
-    this.breadCrumb = this.breadCrumb || { title: 'Bekendmakingen' };
+    this.breadCrumb = this.breadCrumb ||{ title: 'Bekendmakingen' };
   },
 
   model() {
+       this.breadCrumb = { title: 'Bekendmakingen' };
     const id = this.modelFor('bestuurseenheid.zitting').get('id');
     return this.store.findRecord('zitting', id, {
       // TODO add pagination in template instead of retrieving besluiten through include
