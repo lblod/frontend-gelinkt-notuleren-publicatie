c41b87cb43617562e92da3c809c83e31ea31e695
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-03-28 15:29:46 +0100

    adding components to fetch linked uittreksels from model

---
+++ app/components/besluit-overview-item.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+
+export default Component.extend({
+  tagName: 'li',
+  store: service(),
+  findUittreksel: task(function *(){
+    let uittreksels = yield this.store.query('uittreksel', {
+      'filter[behandeling-van-agendapunt][besluiten][id]': this.besluit.id
+    });
+    this.set('uittreksel', uittreksels.firstObject);
+  }),
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.findUittreksel.perform();
+  }
+});
