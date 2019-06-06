a3a68d9698750248747a41ddeaf1ee4b3398f48c
Claire Lovisa <lovisa.claire@gmail.com>
2019-06-06 16:39:27 +0200

    Adapt notulen model

--- app/routes/bestuurseenheid/zitting/notulen.js
+++ app/routes/bestuurseenheid/zitting/notulen.js
@@ -6,7 +6,12 @@ export default Route.extend({
     this.breadCrumb = this.breadCrumb || { title: 'Notulen' };
   },
 
-  model() {
-    return this.modelFor('bestuurseenheid.zitting');
+  async model() {
+    const id = this.modelFor('bestuurseenheid.zitting').get('id');
+    return (await this.store.query('zitting', {
+      // TODO add pagination in template instead of retrieving agendapunten through include
+      'filter[id]': id,
+      include: 'notulen'
+    })).get('firstObject');
   }
 });
