4c8d60bc7c10fecf072ee093877f9c90f6755bce
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-04 17:06:47 +0200

    <SelectBestuurseenheidClassificatieLabel> component improvements
    - colocate the template
    - fix linting errors
    - remove unneeded yield

--- app/components/select-bestuurseenheid-classificatie-label.js
+++ app/components/select-bestuurseenheid-classificatie-label.js
@@ -1,14 +1,16 @@
+/* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/no-component-lifecycle-hooks */
 import Component from '@ember/component';
 import { task } from 'ember-concurrency';
 
 export default Component.extend({
-    findbestuurseenheidClassificatie: task(function * () {
-      const bestuurseenheidClassificatie = yield this.model.map((bestuurseenheid) => ({value: bestuurseenheid.id, label: bestuurseenheid.label}));
-      this.set('bestuurseenheidClassificatie', bestuurseenheidClassificatie);
-    }),
+  tagName: '',
+  findbestuurseenheidClassificatie: task(function * () {
+    const bestuurseenheidClassificatie = yield this.model.map((bestuurseenheid) => ({value: bestuurseenheid.id, label: bestuurseenheid.label}));
+    this.set('bestuurseenheidClassificatie', bestuurseenheidClassificatie);
+  }),
 
-    didReceiveAttrs() {
-      this._super(...arguments);
-      this.findbestuurseenheidClassificatie.perform();
-    }
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.findbestuurseenheidClassificatie.perform();
+  }
 });
