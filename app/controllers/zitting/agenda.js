6aafcd4b884a137f248bde6dd821da580d1f1097
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-06 10:06:10 +0200

    Remove the ZittingAgendaController
    This route does not exist.

--- app/controllers/zitting/agenda.js
+++
@@ -1,7 +0,0 @@
-import Controller from '@ember/controller';
-import { sort } from '@ember/object/computed';
-
-export default class ZittingAgendaController extends Controller {
-  agendaSort = ['position'];
-  @sort('model.agenda.agendapunten', 'agendaSort') agendapunten;
-}
