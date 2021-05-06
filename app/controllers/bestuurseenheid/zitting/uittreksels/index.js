9ad511c7457e994d0d786e36a9426509540fafeb
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-06 14:06:06 +0200

    Fix more linting errors

--- app/controllers/bestuurseenheid/zitting/uittreksels/index.js
+++ app/controllers/bestuurseenheid/zitting/uittreksels/index.js
@@ -1,7 +1,12 @@
+/* eslint-disable ember/no-computed-properties-in-native-classes */
 import Controller from '@ember/controller';
 import { sort } from '@ember/object/computed';
 
 export default class BestuurseenheidZittingUittrekselsIndexController extends Controller {
+  get zitting() {
+    return this.model;
+  }
+
   bvapSort = [
     'behandelingVanAgendapunt.position',
     'behandelingVanAgendapunt.onderwerp.position'
