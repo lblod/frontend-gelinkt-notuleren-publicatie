f08309a7b4d3db4ff48eac67f0560f486cc04858
Niels Vandekeybus <progster@gmail.com>
2021-06-10 21:00:38 +0200

    add publication date to uittreksels

--- app/models/uittreksel.js
+++ app/models/uittreksel.js
@@ -4,7 +4,7 @@ export default class UittrekselModel extends Model {
   @attr uri;
   @attr inhoud;
   @belongsTo('behandeling-van-agendapunt') behandelingVanAgendapunt;
-
+  @belongsTo('published-resource') publication;
   type = 'http://data.lblod.info/id/document-types/uittreksel';
 
   rdfaBindings = {
