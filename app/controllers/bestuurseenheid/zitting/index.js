87c69f8e2026816ee3a2bdc4f30171a5082c7d65
Oscar <lagartoverde97@gmail.com>
2021-10-20 11:43:06 +0200

    Remove publication date

--- app/controllers/bestuurseenheid/zitting/index.js
+++ app/controllers/bestuurseenheid/zitting/index.js
@@ -1,18 +1,8 @@
 /* eslint-disable ember/no-computed-properties-in-native-classes */
 import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed';
 
 export default class BestuurseenheidZittingIndexController extends Controller {
   get zitting() {
     return this.model;
   }
-
-  @alias('model.agendapunten.firstObject.publications.firstObject.createdOn')
-  agendaPublicationDate;
-
-  @alias('model.besluiten.firstObject.publications.firstObject.createdOn')
-  besluitenlijstPublicationDate;
-
-  @alias('model.notulen.publication.createdOn')
-  notulenPublicationDate;
 }
