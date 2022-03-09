635149e422d583b391b783b28000368f82358fca
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-03-09 01:43:55 +0100

    Make tests succeed by removing useless tests
    Most of these test files refer to controllers that don't even exist.

--- tests/unit/routes/bestuurseenheid/zitting/zitting-test.js
+++ tests/unit/routes/bestuurseenheid/zitting/zitting-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | bestuurseenheid/zitting/zitting', function (hooks) {
+module('Unit | Route | bestuurseenheid/zitting/index', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:bestuurseenheid/zitting/zitting');
+    let route = this.owner.lookup('route:bestuurseenheid/zitting/index');
     assert.ok(route);
   });
 });
