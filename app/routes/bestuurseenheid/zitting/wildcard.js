62128d7c791bd1bbeb1aa2d84512c84defef969c
Elena Poelman <elena.poelman@redpencil.io>
2023-07-18 09:07:12 +0200

    fix name of wildcard route and remove unnecessary model hook

--- app/routes/bestuurseenheid/zitting/wildcard.js
+++ app/routes/bestuurseenheid/zitting/wildcard.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 // Route created for backwards compatibility, redirects to `bestuurseenheid.zittingen.zitting`
-export default class BestuurseenheidZittingIndexRoute extends Route {
+export default class BestuurseenheidZittingWildcardRoute extends Route {
   @service router;
 
   beforeModel(transition) {
@@ -13,9 +13,5 @@ export default class BestuurseenheidZittingIndexRoute extends Route {
     const { zitting_id } = this.paramsFor('bestuurseenheid.zitting');
     const url = `${baseUrl}/zittingen/${zitting_id}/${transition.to.params.path}`;
     this.router.replaceWith(url);
-  }
-
-  model() {
-    return;
   }
 }
