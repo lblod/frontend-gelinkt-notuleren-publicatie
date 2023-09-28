d9ff21a7a64338b76f3889a76823bc6cae57d6f2
Elena Poelman <elena.poelman@redpencil.io>
2023-09-28 16:46:00 +0200

    enable embroider

--- app/router.js
+++ app/router.js
@@ -1,4 +1,4 @@
-import EmberRouter from '@ember/routing/router';
+import EmberRouter from '@embroider/router';
 import config from 'frontend-gelinkt-notuleren-publicatie/config/environment';
 
 export default class Router extends EmberRouter {
