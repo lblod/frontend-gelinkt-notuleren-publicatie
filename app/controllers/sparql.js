e13988997e1c63c4d72078150bfe45497315c53a
Elena Poelman <elena.poelman@redpencil.io>
2023-10-02 11:30:20 +0200

    import yasgui modifier by means of a dynamic import

--- app/controllers/sparql.js
+++ app/controllers/sparql.js
@@ -1,6 +1,12 @@
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
-
+import { trackedFunction } from 'ember-resources/util/function';
 export default class SparqlController extends Controller {
   @service fastboot;
+
+  yasgui = trackedFunction(this, async () => {
+    if (!this.fastboot.isFastBoot) {
+      return (await import('../modifiers/yasgui')).default;
+    }
+  });
 }
