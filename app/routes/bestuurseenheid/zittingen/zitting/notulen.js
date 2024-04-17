295d7f3d873ea45fceea327e82887b1797846ddb
abeforgit <arnebertrand@gmail.com>
2024-04-17 23:54:54 +0200

    feat: support notulen being files

--- app/routes/bestuurseenheid/zittingen/zitting/notulen.js
+++ app/routes/bestuurseenheid/zittingen/zitting/notulen.js
@@ -6,11 +6,28 @@ export default class BestuurseenheidZittingenZittingNotulenRoute extends Route {
 
   async model() {
     const parentMeeting = this.modelFor('bestuurseenheid.zittingen.zitting');
+    // TODO seems like the extra load is maybe not needed as it should be loaded through the
+    // parent route
     const meeting = await this.store.findRecord('zitting', parentMeeting.id, {
       reload: true,
       sort: '-notulen.publication.created-on',
-      include: 'notulen,notulen.publication',
+      include: 'notulen,notulen.publication,notulen.file',
     });
-    return meeting;
+    const notulen = await meeting.notulen;
+
+
+
+    const fileMeta = await notulen.file;
+    console.log("fileMeta", fileMeta);
+    let notulenContent;
+    if (fileMeta) {
+      notulenContent = await (
+        await fetch(fileMeta.downloadLink)
+      ).text();
+
+    } else {
+      notulenContent = notulen.inhoud;
+    }
+    return {meeting, notulen, notulenContent};
   }
 }
