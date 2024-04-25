fa2573a1006e4de60654c5dcdff135481ef5b9b1
Aad Versteden <madnificent@gmail.com>
2024-04-25 18:05:33 +0200

    fix(logging) Don't log error object to console directly
    It seems that objects which get logged to the console directly are kept
    around.  This creates a memory leak which could appear on stacks running
    on fastboot.

--- app/routes/error.js
+++ app/routes/error.js
@@ -4,7 +4,7 @@ export default class ErrorRoute extends Route {
   // note model hooks are not called on error routes
   setupController(controller, error) {
     if (error) {
-      console.error(error);
+      console.error(JSON.stringify(error));
       controller.errorMessage = error.message;
     }
     super.setupController(...arguments);
