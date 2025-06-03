5ebb4aa44262596e8f77bfed305c5f4f995b37dc
Elena Poelman <elena.poelman@redpencil.io>
2025-06-03 09:49:35 +0200

    refactor: set prefix and vocab attrs using modifiers

---
+++ .changeset/nasty-kangaroos-refuse.md
@@ -0,0 +1,5 @@
+---
+'frontend-gelinkt-notuleren-publicatie': patch
+---
+
+Set `prefix` and `vocab` attributes using modifiers
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,47 @@
 import Controller from '@ember/controller';
 import { getOwner } from '@ember/application';
 import { service } from '@ember/service';
+import { modifier } from 'ember-modifier';
 
+const PREFIXES = {
+  lblod: 'http://data.lblod.info/vocabularies/lblod/',
+  eli: 'http://data.europa.eu/eli/ontology#',
+  prov: 'http://www.w3.org/ns/prov#',
+  mandaat: 'http://data.vlaanderen.be/ns/mandaat#',
+  besluit: 'http://data.vlaanderen.be/ns/besluit#',
+  generiek: 'http://data.vlaanderen.be/ns/generiek#',
+  person: 'http://www.w3.org/ns/person#',
+  persoon: 'http://data.vlaanderen.be/ns/persoon#',
+  dct: 'http://purl.org/dc/terms/',
+  skos: 'http://www.w3.org/2004/02/skos/core#',
+  org: 'http://www.w3.org/ns/org#',
+  foaf: 'http://xmlns.com/foaf/0.1/',
+  ext: 'http://mu.semte.ch/vocabularies/ext/',
+  besluittype: 'https://data.vlaanderen.be/id/concept/BesluitType/',
+  lblodBesluit: 'http://lblod.data.gift/vocabularies/besluit/',
+};
+
+const VOCAB = 'http://data.vlaanderen.be/ns/besluit#';
 export default class ApplicationController extends Controller {
+  setPrefixes = modifier(
+    (element) => {
+      const prefixString = Object.entries(PREFIXES)
+        .map(([prefix, uri]) => {
+          return `${prefix}: ${uri}`;
+        })
+        .join(' ');
+      element.setAttribute('prefix', prefixString);
+    },
+    { eager: false }
+  );
+
+  setVocab = modifier(
+    (element) => {
+      element.setAttribute('vocab', VOCAB);
+    },
+    { eager: false }
+  );
+
   @service router;
   get environmentName() {
     return getOwner(this).resolveRegistration('config:environment')
