9b5a186b127c788d0c398f57c3b78aa6f78219d7
abeforgit <arnebertrand@gmail.com>
2026-07-27 22:17:03 +0200

    chore: remove debug print statements

--- app/controllers/bestuurseenheid/zittingen/zitting/agenda/index.js
+++ app/controllers/bestuurseenheid/zittingen/zitting/agenda/index.js
@@ -10,7 +10,6 @@ export default class BestuurseenheidZittingenZittingAgendaIndexController extend
   }
 
   get publication() {
-    console.info('DEBUGPRINT[16]: index.js:13 (after get publication() )');
     return this.model.agenda.publication;
   }
 }
